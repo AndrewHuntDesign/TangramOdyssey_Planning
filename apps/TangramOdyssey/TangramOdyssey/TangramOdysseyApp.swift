f3b89253ca23debd2132f276162334ca4f068a45
Andy Hunt <andyh@wolfram.com>
2026-07-19 11:42:41 -0600

    Commit blank xcode files

---
+++ apps/TangramOdyssey/TangramOdyssey/TangramOdysseyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangramOdysseyApp.swift
+//  TangramOdyssey
+//
+//  Created by Andy Hunt on 7/19/26.
+//
+
+import SwiftUI
+
+@main
+struct TangramOdysseyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
